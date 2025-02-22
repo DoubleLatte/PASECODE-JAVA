1f915b90e4eb3ea9d13564de70986768c6493ffb
DoubleLatte <85533915+DoubleLatte@users.noreply.github.com>
2025-02-22 13:25:21 +0900

    Create EncryptedFileSystem.java

---
+++ com/example/encryption/EncryptedFileSystem.java
@@ -0,0 +1,20 @@
+package com.example.encryption;
+
+// 이 클래스는 제공된 코드에 정의되지 않았으므로 예상 구조만 작성
+public class EncryptedFileSystem {
+    public void generateKey(String keyFilePath, String password) throws Exception {
+        // 키 생성 로직 구현
+    }
+    
+    public void loadKey(String keyFilePath, String password) throws Exception {
+        // 키 로드 로직 구현
+    }
+    
+    public void encryptFile(String filePath, int chunkSize) throws Exception {
+        // 파일 암호화 로직 구현
+    }
+    
+    public void decryptFile(String filePath) throws Exception {
+        // 파일 복호화 로직 구현
+    }
+}
